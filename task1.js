ed2170fe3434b411c407ab605e265c55009f0e73
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-02-17 21:31:12 +0300

    feat: task1 solving

---
+++ task1.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+  const result = +a + +b;
+  if ((typeof a === 'number' || typeof a === 'string') &&
+  (typeof b === 'number' || typeof b === 'string') &&
+  (!Number.isNaN(result))) {
+    return Number.isInteger(result)
+      ? result
+      : +(result).toFixed(3);
+  }
+  return 'Ошибка! Аргументы функции должны быть числами.';
+};
